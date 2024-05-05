b3da7316d6b7408f3813f995f11d34e78ceedd33
huhututu0599 <1534677036@qq.com>
2024-05-06 00:40:56 +0800

    1
    第一次提交

---
+++ ruoyi-ui/src/systemConfig.js
@@ -0,0 +1,70 @@
+﻿
+
+window.menu={
+  "role":"1-1",
+  "user":"1-2",
+  "right":"1-3",
+  "loginLog":"1-4",
+
+  "params":"2-1",
+  "partner":"2-2",
+  "uom":"2-3",
+  "sku":"2-4",
+  "material":"2-5",
+  "warehouseArea":"2-6",
+  "warehouseLocation":"2-7",
+  "operator":"2-8",
+  "operatorWork":"2-9",
+  "province":"2-10",
+  "baseworkbench":"2-11",
+
+  "putaway":"3-1",
+  "allocate":"3-2",
+  "replenishment":"3-3",
+  "barcodeDescription":"3-4",
+  "barcodesGenerated":"3-5",
+
+  "purchase":"4-1",
+  "recReceipt":"4-2",
+
+  "cretificate":"5-1",
+  "needCretificate":"5-2",
+
+  "equipmentStatus":"6-1",
+
+  "storageLotAttribute":"7-1",
+  "inventoryRecord":"7-2",
+  "storageTaskDetail":"7-3",
+  "inventoryOtherData":"7-4",
+
+  "shipMentOrder":"8-1",
+  "shipPickDetail":"8-2",
+  "appointAssignment":"8-3",
+
+  "serviceRequest":"9-1",
+  "wareHouseNotice":"9-2",
+  "signature":"9-3",
+
+  "outboundOrder":"10-1",
+  "inventory":"10-2",
+  "command":"10-3",
+  "check":"10-4",
+
+  "wmsLog":"11-1",
+  "agvLog":"11-2",
+  "traceLog":"11-3",
+  "plcLog":"11-4",
+  "printerLog":"11-5",
+
+  "task":"12-1",
+  "taskLog":"12-1",
+
+  "set":"13-1",
+
+};
+
+
+export const IpPort="192.168.124.8:8080";
+console.log("内部打印↓")
+console.log(IpPort.toString());
+console.log("内部打印↑")
